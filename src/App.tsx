af258ccaf9ab33b9cf97dd26b0972aab58fdb7a0
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-11-02 13:45:49 -0700

    Build the main content of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import logo from "./assets/logo.svg";
+import illustration from "./assets/illustration-dashboard.png";
 
 function App() {
   return (
@@ -6,7 +7,25 @@ function App() {
       <header>
         <img src={logo} alt="logo" />
       </header>
-      <main></main>
+      <main>
+        <section className="content">
+          <div className="titleDescContainer">
+            <h1 className="title">
+              We are launching <span>soon!</span>
+            </h1>
+            <p className="desc">Subscribe and get notified</p>
+          </div>
+          <form>
+            <div className="formGroup">
+              <input type="email" placeholder="Your email address..." />
+            </div>
+            <button className="submitBtn">Notify Me</button>
+          </form>
+        </section>
+        <section className="illustrationImageContainer">
+          <img src={illustration} alt="illustration" />
+        </section>
+      </main>
       <footer></footer>
     </div>
   );
